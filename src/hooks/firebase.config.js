1bdf48cdcfa1d7ec218b1dfd8e06b63cab38e944
mamunarr6 <mamunarr6@gmail.com>
2021-10-18 18:38:09 +0600

    first commit

---
+++ src/hooks/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeoaR7gyWrdBhJFFsJdlR0orW7s4qHQWc",
+    authDomain: "health-cure-hospital.firebaseapp.com",
+    projectId: "health-cure-hospital",
+    storageBucket: "health-cure-hospital.appspot.com",
+    messagingSenderId: "399055910226",
+    appId: "1:399055910226:web:988cf8fe337ebc2874b68b"
+};
+
+export default firebaseConfig;
